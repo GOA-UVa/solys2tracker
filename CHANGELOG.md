13dd73ec749dcb0aa7db02b9073d937bf8ef1583
Javier Gatón Herguedas <gaton@goa.uva.es>
2025-09-12 12:21:40 +0200

    using appdata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 - Using solys2==v0.2.7
+- Using appdata
 
 ### Fixed
 - BodyWidget was accessing its graph attribute even if no graph had been generated (not using ASD).
--- solys2tracker/localdata.py
+++ solys2tracker/localdata.py
@@ -1,31 +1,100 @@
-"""___Built-In Modules___"""
+import os
+import sys
 import pickle
 from typing import Dict
+import pathlib
 
-"""___Third-Party Modules___"""
-# import here
-
-"""___Solys2Tracker Modules___"""
 try:
     from solys2tracker import constants
-except:
+except Exception:
     import constants
+
+
+
+def _is_valid_appdata(appdata: str) -> bool:
+    """Checks if a given appdata path is valid, and tries to modify it if not so it is.
+
+    Parameters
+    ----------
+    appdata: str
+        Appdata folder absolute path
+
+    Returns
+    -------
+    valid: bool
+        Validity of the appdata path.
+    """
+    if not os.path.exists(appdata):
+        try:
+            os.makedirs(appdata)
+        except Exception:
+            return False
+    return True
+
+
+def _get_appdata_folder(platform: str) -> str:
+    """Find the theoretical path of the appdata folder for a given platform. Function useful for testing.
+
+    Parameters
+    ----------
+    platform: str
+        System OS platform (darwin, win32 or linux)
+
+    Returns
+    -------
+    appdata_path: str
+        Appdata folder absolute path for that platform
+    """
+    if platform == "darwin":
+        home = pathlib.Path.home()
+        appdata = str(home / "Library/Application Support" / constants.APPLICATION_NAME)
+    elif platform == "win32":
+        appdata = os.path.join(
+            os.environ.get("APPDATA", os.path.join(os.getcwd(), "appdata")),
+            constants.APPLICATION_NAME,
+        )
+    else:
+        appdata = os.path.expanduser(os.path.join("~", "." + constants.APPLICATION_NAME))
+    return appdata
+
+
+def get_appdata_folder() -> str:
+    """Find the path of the appdata folder
+
+    Returns
+    -------
+    appdata_path: str
+        Appdata folder absolute path
+    """
+    platf = sys.platform
+    appdata = _get_appdata_folder(platf)
+    if not _is_valid_appdata(appdata):
+        print("Appdata folder not valid, using '.'")
+        appdata = "."
+    return appdata
+
+
+def get_data_path() -> str:
+    os.path.join(get_appdata_folder(), constants.DATA_FILE_PATH)
+
 
 def save_value(key: str, value: str):
     d = get_all_values()
     d[key] = value
-    f = open(constants.DATA_FILE_PATH, "wb")
+    f = open(get_data_path(), "wb")
     pickle.dump(d, f)
     f.close()
 
+
 def get_all_values() -> Dict[str, str]:
     try:
-        f=open(constants.DATA_FILE_PATH,"rb")
+        f=open(get_data_path(),"rb")
         d=pickle.load(f)
         f.close()
-        return d
-    except:
-        return {}
+    except Exception:
+        d = {}
+    return d
+
 
 def get_value(key: str) -> str:
     d = get_all_values()
